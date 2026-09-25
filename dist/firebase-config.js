a75fd7426b1f23a9cb91880f7b08dcfb6f503de4
kleberantunes <djkleber@gmail.com>
2026-09-24 21:28:31 -0300

    feat: configurar projeto Firebase intensivo-sesi-rs-2026 e regras do Firestore

--- dist/firebase-config.js
+++ dist/firebase-config.js
@@ -1,13 +1,9 @@
-// Arquivo de configuração ativo do Firebase.
-// Substitua os valores abaixo pelos dados do seu projeto no Firebase Console.
-// Se as chaves padrão forem mantidas, o app continuará funcionando localmente (localStorage)
-// e exibirá orientações ao clicar em "Entrar com o Google".
-
+// Configuração ativa do Firebase para o Intensivo para SESI-RS
 window.FIREBASE_CONFIG = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "seu-projeto",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  apiKey: "AIzaSyDzXdKXEDFqlxrPnswRzXsOwtREN8rQOYE",
+  authDomain: "intensivo-sesi-rs-2026.firebaseapp.com",
+  projectId: "intensivo-sesi-rs-2026",
+  storageBucket: "intensivo-sesi-rs-2026.firebasestorage.app",
+  messagingSenderId: "966380159873",
+  appId: "1:966380159873:web:1664e0e17fc01cccedde80"
 };
